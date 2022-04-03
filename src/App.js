501c7de537cd2a506a157c54f21749568777fc66
Valentin Charrier <valentin.charrier@hotmail.fr>
2022-04-03 20:42:32 +0200

    Déploy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [keyboard, setKeyboard] = useState({});
+  useEffect(() => {
+    window.addEventListener('keyup', (event) => {
+      event.preventDefault();
+      setKeyboard(event);
+    });
+  }, [keyboard]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Seeboard Catching</h1>
+      <p className='alert info'>Click sur une touche clavier ⌨ pour voir ses informations !</p>
+      { keyboard != {} && (
+        <ul>
+          <li>
+            { keyboard.altKey  && ("alt + ")}
+            { keyboard.ctrlKey  && ("ctrl + ")}
+            { keyboard.shiftKey  && ("shift + ")}
+            { keyboard.code }
+          </li>
+          <li>keyCode: { keyboard.keyCode }</li>
+        </ul>
+      )}
     </div>
   );
 }
