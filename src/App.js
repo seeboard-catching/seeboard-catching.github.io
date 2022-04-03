5507911f86358d775a31452f9e577c24f6485d47
Valentin Charrier <valentin.charrier@hotmail.fr>
2022-04-03 21:42:09 +0200

    Fix Ajout de fichier manquants

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [keyboard, setKeyboard] = useState({});
+  useEffect(() => {
+    window.addEventListener('keyup', (event) => {
+      event.preventDefault();
+      setKeyboard(event);
+    });
+  }, [keyboard]);
+  return (
+    <div className="App">
+      <h1>Seeboard Catching</h1>
+      <p className='alert info'>Click sur une touche clavier ⌨ pour voir ses informations !</p>
+      { Object.keys(keyboard).length !== 0 && (
+        <ul>
+          <li>
+            { keyboard.altKey  && ("alt + ")}
+            { keyboard.ctrlKey  && ("ctrl + ")}
+            { keyboard.shiftKey  && ("shift + ")}
+            { keyboard.code }
+          </li>
+          <li>keyCode: { keyboard.keyCode }</li>
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
